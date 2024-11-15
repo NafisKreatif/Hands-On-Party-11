19331f3e3ac0dbfd03915dea9744508868382ee3
Shui <faiq.azzam@gmail.com>
2024-11-15 18:09:14 +0700

    Added Camera Movement
    Camera moves to a target with the velocity based on how long the distance between them.

---
+++ Assets/Scripts/toMouse.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class toMouse : MonoBehaviour
+{
+    public float speed;
+    Vector3 mousePos;
+
+	void Start ()
+    {
+        
+	}
+	
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.Mouse1))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        transform.position = new Vector3 (mousePos.x, mousePos.y, 0);
+    }
+}
