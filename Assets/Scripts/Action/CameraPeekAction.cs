6c53323923e859b45a1b84c0674c351febf8f036
Cikang44 <jingglang@gmail.com>
2024-11-23 16:01:54 +0700

    Modified CameraPeekAction to be able to zoom

--- Assets/Scripts/Action/CameraPeekAction.cs
+++ Assets/Scripts/Action/CameraPeekAction.cs
@@ -7,13 +7,16 @@ public class CameraPeekAction : SceneAction
 
   [Tooltip("Duration of the peeking in seconds.")]
   public float duration = 1.0f;
+  public float zoomFactor = 1.0f;
   public GameObject targetObject;
 
   private CameraMovement _cameraMovement;
+  private Camera _camera;
 
   public void Start()
   {
     _cameraMovement = FindFirstObjectByType<CameraMovement>();
+    _camera = _cameraMovement.GetComponent<Camera>();
   }
 
   public override void Execute(string id)
@@ -27,9 +30,24 @@ public class CameraPeekAction : SceneAction
   {
     GameObject tempTarget = _cameraMovement.target;
     _cameraMovement.target = targetObject;
+    float originalOrthographicSize = _camera.orthographicSize;
+    float time = 0;
+    while (time <= duration / 2)
+    {
+      _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, originalOrthographicSize * zoomFactor, time / (duration / 2));      
+      time += Time.deltaTime;
+      yield return null;
+    }
 
-    yield return new WaitForSeconds(duration / 2);
     _cameraMovement.target = tempTarget;
+    time = 0;
+    while (time <= duration / 2)
+    {
+      _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, originalOrthographicSize, time / (duration / 2));
+      time += Time.deltaTime;
+      yield return null;
+    }
+
 
     yield return new WaitForSeconds(duration / 2);
     DialogManager.Instance.DialogDone(dialogId);
