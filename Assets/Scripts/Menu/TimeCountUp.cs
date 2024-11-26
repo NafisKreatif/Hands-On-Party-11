0b92a6a655cb7762c42d08fcdfc5e997b02ba483
Shui <faiq.azzam@gmail.com>
2024-11-26 20:11:00 +0700

    Fixed animation not working (hopefully)
    it's fine on my laptop!!!

--- Assets/Scripts/Menu/TimeCountUp.cs
+++ Assets/Scripts/Menu/TimeCountUp.cs
@@ -4,24 +4,25 @@ public class TimeCountUp : MonoBehaviour
 {
     public LevelCompletedMenu winMenu;
     public int timeCompleted = 0;
-    private int _growAmount = 0;
-    private int _timeCountUp = 0;
+    public int growAmount = 1;
+    public int timeCountUp;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
+        timeCountUp = 0;
         timeCompleted = Mathf.RoundToInt(Time.timeSinceLevelLoad * 1000f);
+        while(timeCountUp <= timeCompleted - growAmount){
+            timeCountUp += growAmount;
+            winMenu.SetWinTime(timeCountUp);
+        }
+        timeCountUp = timeCompleted;
+        winMenu.SetWinTime(timeCountUp);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_timeCountUp < timeCompleted - _growAmount){
-            _timeCountUp += _growAmount;
-        }
-        else{
-            _timeCountUp = timeCompleted;
-        }
-        winMenu.SetWinTime(_timeCountUp);
+        
     }
 }
