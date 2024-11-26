ea2af02d1edd617d720a3aa1dc892d51f16ee0c7
Shui <faiq.azzam@gmail.com>
2024-11-26 21:10:55 +0700

    Added Time Text, Best Text, and Orb Animation
    maybe need some adjustment with the timing of the best text and orb animation timing. I tried to make the text time animation to be about 3 seconds long.

--- Assets/Scripts/Menu/TimeCountUp.cs
+++ Assets/Scripts/Menu/TimeCountUp.cs
@@ -3,26 +3,33 @@ using UnityEngine;
 public class TimeCountUp : MonoBehaviour
 {
     public LevelCompletedMenu winMenu;
+    public WinController winController;
     public int timeCompleted = 0;
-    public int growAmount = 1;
+    public int growAmount;
     public int timeCountUp;
+    private bool _isEnabled = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
         timeCountUp = 0;
-        timeCompleted = Mathf.RoundToInt(Time.timeSinceLevelLoad * 1000f);
-        while(timeCountUp <= timeCompleted - growAmount){
-            timeCountUp += growAmount;
-            winMenu.SetWinTime(timeCountUp);
-        }
-        timeCountUp = timeCompleted;
-        winMenu.SetWinTime(timeCountUp);
+        timeCompleted = winController.timeInMiliseconds;
+        growAmount = Mathf.RoundToInt(timeCompleted/180);
+        _isEnabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_isEnabled){
+            if(timeCountUp <= timeCompleted - growAmount){
+                timeCountUp += growAmount;
+                winMenu.SetWinTime(timeCountUp);
+            }
+            else{
+                timeCountUp = timeCompleted;
+                winMenu.SetWinTime(timeCountUp);
+            }
+        }
     }
 }
