7a0ecd0b09c1522036ae65e59b8108f1c1f8c828
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2025-01-17 17:05:31 +0700

    More Accurate Boss Hitbox

--- Assets/Scripts/Boss/BossAnimation.cs
+++ Assets/Scripts/Boss/BossAnimation.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class Boss_animation : MonoBehaviour
+public class BossAnimation : MonoBehaviour
 {
     public Transform leftWing;
     public Transform rightWing;
@@ -10,10 +10,10 @@ public class Boss_animation : MonoBehaviour
     public float bossSpeed = 0.3f;
     public float startAccelerateDistance = 5f;
     public float actualSpeed = 0.3f;
-    public float flapSpeed = 24f;
+    public float flapFrequency = 0.6f;
     public float xFlapAngle = 0.8f;
     public float zFlapAngle = 8f;
-    public float headRotationSpeed = 0.8f;
+    public float headRotationFrequency = 0.6f;
     public float headAngle = -6f;
     private float _xRotation;
     private float _zRotation;
@@ -27,9 +27,9 @@ public class Boss_animation : MonoBehaviour
         Vector2 bossPosition = transform.position;
         Vector2 playerPosition = _playerTranform.position;
         actualSpeed = Math.Max(bossSpeed, ((playerPosition - bossPosition).magnitude - startAccelerateDistance + 1) * bossSpeed);
-        _xRotation = Mathf.Sin(Time.time * flapSpeed) * xFlapAngle;
-        _zRotation = Mathf.Sin(Time.time * flapSpeed) * zFlapAngle;
-        head.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * headRotationSpeed) * headAngle);
+        _xRotation = Mathf.Sin(Time.time * flapFrequency * 2 * Mathf.PI) * xFlapAngle;
+        _zRotation = Mathf.Sin(Time.time * flapFrequency * 2 * Mathf.PI) * zFlapAngle;
+        head.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * headRotationFrequency * 2 * Mathf.PI) * headAngle);
         leftWing.localRotation = Quaternion.Euler(_xRotation, 0, _zRotation);
         rightWing.localRotation = Quaternion.Euler(-_xRotation, 0, -_zRotation);
         transform.position += new Vector3(0, actualSpeed * Time.deltaTime, 0);
@@ -38,8 +38,5 @@ public class Boss_animation : MonoBehaviour
     public void SetSpeed(float newSpeed)
     {
         bossSpeed = newSpeed;
-    }
-    public void SetWalking(bool isWalking) {
-        
     }
 }
--- Assets/Scripts/Level Objects/HostileMobMovements.cs
+++ Assets/Scripts/Level Objects/HostileMobMovements.cs
@@ -1,25 +1,24 @@
 using UnityEngine;
 
-public class Hostile_mob_movements : MonoBehaviour
+public class HostileMobMovements : MonoBehaviour
 {
     // function LeftRightMovement menggerakkan gameobj ke kiri-kanan-kiri-kanan... parameternya 
     // time dan distance, time = waktu yang dibutuhkan untuk mencapai distance, distance = jarak yang ingin ditempuh saat ke kiri maupun kanan
     // UpDownMovemenet juga sama aja cuma gerakannya atas bwah
     // kecepatannya angular, kalau mau linear bisa ganti speednya jadi linear-sinusoidal function (aku gatau itu ada ato gak tp you get the idea)
     public float speed;
-    public Rigidbody2D rb;
-    private Vector3 startPosition; 
+    private Vector3 _startPosition; 
     void Start()
     {
-        startPosition = transform.position;
+        _startPosition = transform.position;
     }
     void Update()
     {
-        float xOffset = LeftRightMovement(5,5);
-        float yOffset = UpDownMovement(5,5);   
-        transform.position = new Vector3(startPosition.x + xOffset, startPosition.y + yOffset, startPosition.z);
+        float xOffset = LeftRightMovement(5, 5);
+        float yOffset = UpDownMovement(5, 5);   
+        transform.position = new Vector3(_startPosition.x + xOffset, _startPosition.y + yOffset, _startPosition.z);
     }
-    float LeftRightMovement(float time,float distance)
+    float LeftRightMovement(float time, float distance)
     {
         float speed = 2 * Mathf.PI / time;
         float offset = Mathf.Sin(Time.time * speed) * distance;
